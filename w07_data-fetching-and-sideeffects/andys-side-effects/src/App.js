e5eaeacb9c897676897b8ee9b8a430bc3113840c
Christian Nally <sticksallison@gmail.com>
2023-01-12 09:22:51 -0800

    Side Effects and Data Fetching

---
+++ w07_data-fetching-and-sideeffects/andys-side-effects/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import Counter from './components/Counter';
+import FetchData from './components/FetchData';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Fun with useEffect!</h2>
+      {/* <Counter /> */}
+      <FetchData />
+    </div>
+  );
+};
+
+export default App;
